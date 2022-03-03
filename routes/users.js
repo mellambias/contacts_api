0c8e68cf7d404ee3d0ea99f3d152daf773fd6080
mellambias <mellambias@gmail.com>
2022-03-03 12:11:02 +0100

    validation

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
+const config = require("config");
+const {check,validationResult} = require("express-validator");
 
 const router = express.Router();
 
@@ -9,43 +11,55 @@ const router = express.Router();
 //@desc     Registra un nuevo usuario
 //@access   Public
 
-router.post('/', async (req, res) => {
-    // Validar los datos
+router.post(
+  "/",
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El correo no es valido").not().isEmpty().isEmail(),
+    check("password", "El password debe tener mas de 2").isLength({min: 2})
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
+    }
     // Mirar si el user ya existe
-    
+
     console.log(req.body);
     const { name, email, password } = req.body;
-  try {
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({msg:'El usuario ya existe'});
-    }
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "El usuario ya existe" });
+      }
       user = new User({
         name,
         email,
         password,
       });
-    
+
       // mongoose crea un objeto que coincide con el esquema.
       // Encriptar el password
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
-      
-    await user.save();
-    // generamos un token JWT y lo enviamos
-    const payload = { user: { id: user.id } };
-    jwt.sign(payload, 'secreto', { expiresIn: 3600 }, (err, token) => {
-      if (err) throw err;
-      res.json(token);
-    });
 
-    
-
+      await user.save();
+      // generamos un token JWT y lo enviamos
+      const payload = { user: { id: user.id } };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json(token);
+        },
+      );
     } catch (error) {
-        console.error(error.message);
-        res.status(503).send('Base de datos en mantenimiento');
+      console.error(error.message);
+      res.status(503).send("Base de datos en mantenimiento");
     }
-    
-});
+  },
+);
 
 module.exports = router;
