e62b4f72e006023c22ffa053015d51fd7bb5f7e4
mellambias <mellambias@gmail.com>
2022-03-03 11:42:39 +0100

    jueves

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -7,29 +9,43 @@ const router = express.Router();
 //@desc     Registra un nuevo usuario
 //@access   Public
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // Validar los datos
     // Mirar si el user ya existe
     
     console.log(req.body);
     const { name, email, password } = req.body;
   try {
-    let user = User.findOne({ email });
+    let user = await User.findOne({ email });
     if (user) {
-      res.status(400).json({msg:'El usuario ya existe'});
+      return res.status(400).json({msg:'El usuario ya existe'});
     }
       user = new User({
         name,
         email,
         password,
       });
-        // mongoose crea un objeto que coincide con el esquema.
-      user.save();
+    
+      // mongoose crea un objeto que coincide con el esquema.
+      // Encriptar el password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      
+    await user.save();
+    // generamos un token JWT y lo enviamos
+    const payload = { user: { id: user.id } };
+    jwt.sign(payload, 'secreto', { expiresIn: 3600 }, (err, token) => {
+      if (err) throw err;
+      res.json(token);
+    });
+
+    
+
     } catch (error) {
         console.error(error.message);
-        res.status(500).send('Error en el servidor');
+        res.status(503).send('Base de datos en mantenimiento');
     }
-    res.json(req.body);
+    
 });
 
 module.exports = router;
